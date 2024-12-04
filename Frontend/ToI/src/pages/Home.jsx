b16ff81e7addc875283d9b00fb72fb435940ad8e
Jordi Nakahara <jordi13@my.yorku.ca>
2024-12-03 19:36:39 -0500

    Reorganized and improved frontend
    Moved the crawling and searching functionality from components into pages. You now click a button to go to another page before interacting. Made some stylistic changes to the frontend as well.

--- Frontend/ToI/src/pages/Home.jsx
+++ Frontend/ToI/src/pages/Home.jsx
@@ -8,11 +8,18 @@ import ScrapingComponent from "../components/ScrapingComponent";
 // This is the page that currently serves as the frontend for the searching functionality
 export default function Test() {
   return (
-    <div className="grid grid-cols-1 gap-4">
-      <div>
+    <div className="grid grid-cols-1 pt-48">
+      <div className="pb-10">
         <p className="text-8xl">ToI</p>
-        {/* Below is a react component for searching. */}
-        <ScrapingComponent />
+      </div>
+      <div className="grid grid-cols-3">
+      <a href="/crawl">
+        <button>Webcrawling</button>
+        </a>
+        <p className="px-10"></p>
+        <a href="/search">
+        <button>Search</button>
+        </a>
       </div>
     </div>);
 }
